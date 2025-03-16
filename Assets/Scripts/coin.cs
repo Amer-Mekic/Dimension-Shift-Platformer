e64e3881206cfa5851f3ed0f51b1c260f8529883
Amer Mekic <amerm3kic@gmail.com>
2025-03-16 19:53:10 +0100

    Added test script, sound effect & design to coin (FINALIZED)

--- Assets/Scripts/coin.cs
+++ Assets/Scripts/coin.cs
@@ -2,11 +2,25 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    public AudioClip coinSound;
+    private AudioSource audioSource;
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null) {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+    public void CollectCoin(){
+        Destroy(gameObject, 0.5f); // delay collecting 0.5s to play sound
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Destroy(gameObject); // Remove coin when collected
+            audioSource.PlayOneShot(coinSound); // Play sound
+            GetComponent<MeshRenderer>().enabled = false; // Hide coin
+            GetComponent<Collider>().enabled = false; // Disable collider
+            CollectCoin();
         }
     }
 }
