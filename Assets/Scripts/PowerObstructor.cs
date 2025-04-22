9eba18b77f999ab1100001262e9f9a4a4137a126
Amer Mekic <amerm3kic@gmail.com>
2025-04-22 22:00:11 +0200

    Finished implementing all object logic

--- Assets/Scripts/PowerObstructor.cs
+++ Assets/Scripts/PowerObstructor.cs
@@ -1,12 +1,29 @@
 using UnityEngine;
+using System.Collections;
 
-// We extend our base Collectible class
 public class PowerObstructor : Collectible
 {
-    public float disableDuration = 5f; // Time for which switching is disabled
+    public float disableDuration = 5f; // Set from Inspector to as much seconds as you want
+    private DimensionShift dimensionShift; 
+
     protected override void Collect()
     {
+        // Find and disable dimension shifting mechanism
+        if (dimensionShift == null)
+        {
+            dimensionShift = FindObjectOfType<DimensionShift>();
+        }
+
+        if (dimensionShift != null)
+        {
+            Debug.Log("[PowerObstructor] Disabling Dimension Shift for " + disableDuration + " seconds.");
+            dimensionShift.DisableShift(disableDuration);
+        }
+        else
+        {
+            Debug.LogWarning("[PowerObstructor] DimensionShift script not found!");
+        }
+
         base.Collect();
-        Debug.Log("PO Collected!");
     }
 }
